5b7e8237768f7bb517726f59a98500d345695e95
scheidge <scheidge@d9bd5f76-c820-0410-884a-9a5e71472d2e>
2007-04-19 14:52:37 +0000

    Copied remotely

---
+++ branches/hub.sam.tef.tef1/src/hub/sam/tef/parse/EmptySemantic.java
@@ -0,0 +1,20 @@
+package hub.sam.tef.parse;
+
+import java.util.List;
+
+import fri.patterns.interpreter.parsergenerator.Semantic;
+import fri.patterns.interpreter.parsergenerator.Token.Range;
+import fri.patterns.interpreter.parsergenerator.syntax.Rule;
+
+public class EmptySemantic implements Semantic {
+
+	public Object doSemantic(Rule rule, List parseResults, List<Range> resultRanges) {
+		return null;
+	}
+
+	public Object doSemanticForErrorRecovery(String recoverSymbol) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
