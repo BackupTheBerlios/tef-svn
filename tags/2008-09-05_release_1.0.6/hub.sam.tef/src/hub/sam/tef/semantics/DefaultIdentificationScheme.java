aabe1bd91cba770f7ddf55c505c117a671ade11e
sadilek <sadilek@d9bd5f76-c820-0410-884a-9a5e71472d2e>
2008-09-05 09:00:54 +0000

    Release of TEF 1.0.6.

---
+++ tags/2008-09-05_release_1.0.6/hub.sam.tef/src/hub/sam/tef/semantics/DefaultIdentificationScheme.java
@@ -0,0 +1,31 @@
+package hub.sam.tef.semantics;
+
+import hub.sam.tef.util.EObjectHelper;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * A default identification scheme that relies on possible name attributes of
+ * elements. It does not distinguish between global and local ids, there are not
+ * namespaces or other advanced concepts.
+ */
+public class DefaultIdentificationScheme implements IIdentificationScheme {
+
+	public static DefaultIdentificationScheme INSTANCE = new DefaultIdentificationScheme();
+	
+	public Object[] getGlobalIdentities(Object identifier, EObject context) {
+		if (identifier instanceof EObject) {
+			return new Object[] { EObjectHelper.getLocalId((EObject)identifier) };
+		} else {
+			return new Object[] {identifier};
+		}
+	}
+
+	public Object getIdentitiy(EObject object) {
+		return EObjectHelper.getLocalId(object);
+	}
+
+	public String getLocalIdentity(EObject object, EObject context) {
+		return (String)getIdentitiy(object);
+	}
+}
